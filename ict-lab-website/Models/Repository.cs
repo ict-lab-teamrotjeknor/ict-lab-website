3c7c93225f529f351558535c95b54967ffd2987f
Jeroen <jeroenlafleur@gmail.com>
2018-03-14 21:07:49 +0100

    Add functionality to list Rooms, view their details and create them

---
+++ ict-lab-website/Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ict_lab_website.Models
+{
+    public class Repository
+    {
+        private static List<Room> rooms = new List<Room>();
+
+        public static List<Room> Rooms
+        {
+            get
+            {
+                return rooms;
+            }
+        }
+
+        public static void AddRoom(Room room)
+        {
+            rooms.Add(room);
+        }
+    }
+}
---
+++ ict-lab-website/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ict_lab_website.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string RoomCode { get; set; }
+        public Dictionary<int, string> reservations { get; set; }
+    }
+}
