e10f231c4c783ae0b03f469a49ec09cdca47f454
Wouterkop1993 <0919023@hr.nl>
2018-03-17 13:33:40 +0100

    Deleted unnessecary files

--- ict-lab-website/Controllers/HomeController.cs
+++ ict-lab-website/Controllers/HomeController.cs
@@ -15,16 +15,6 @@ namespace ict_lab_website.Controllers
             return View();
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        public IActionResult Register()
-        {
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
