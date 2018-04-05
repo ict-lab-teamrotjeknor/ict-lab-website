25de4be835453212715d69f16ce1ad720562a504
Jeroen <jeroenlafleur@gmail.com>
2018-04-05 16:51:58 +0200

    Add API-connection

---
+++ ict-lab-website/Models/APIConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ict_lab_website.Models
+{
+    public class APIConnection
+    {
+        private static string GetJsonFromApi(string url)
+        {
+            var json = "";
+            using (var client = new WebClient())
+            {
+                json = client.DownloadString(url);
+            }
+            return json;
+        }
+
+        public static List<Room> GetAllRooms(string url)
+        {
+            List<Room> rooms = new List<Room>();
+            var json = GetJsonFromApi(url);
+            var classRooms = JObject.Parse(json)["Classroom"];
+
+            foreach (JToken classrooms in classRooms)
+            {
+                foreach(JToken room in classrooms.Children())
+                {
+                    Room r = JsonConvert.DeserializeObject<Room>(room.ToString());
+                    rooms.Add(r);
+                }
+
+            }
+            return rooms;
+        }
+    }
+}
--- ict-lab-website/Models/FakeRoomRepository.cs
+++ ict-lab-website/Models/FakeRoomRepository.cs
@@ -12,37 +12,38 @@ namespace ict_lab_website.Models
 
         public FakeRoomRepository()
         {
-            rooms = new List<Room>
-            {
-                new Room
-                {
-                    ID = 1,
-                    RoomCode = "H.4.104",
-                    Reservations = new List<Reservation>
-                    {
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 05, 12, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" },
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 05, 04, 12, 10, 0), Length = 2, Reserver = "MINUTO", Subject = "INFLAB-01" },
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 05, 14, 00, 0), Length = 2, Reserver = "LAFLEJ", Subject = "TESTING" },
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 03, 05, 12, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" },
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 08, 12, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" }
-                    }
-                },
-                new Room
-                {
-                    ID = 2,
-                    RoomCode = "H.4.204",
-                    Reservations = new List<Reservation>
-                    {
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2010, 04, 05, 11, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" },
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2017, 05, 04, 12, 10, 0), Length = 2, Reserver = "MINUTO", Subject = "INFLAB-01" },
-                        new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 05, 14, 00, 0), Length = 2, Reserver = "LAFLEJ", Subject = "TESTING" },
-                    },
-                },
-                new Room { ID = 3, RoomCode = "H.3.304" },
-                new Room { ID = 4, RoomCode = "WD.3.304" },
-                new Room { ID = 5, RoomCode = "H.3.305" },
-                new Room { ID = 6, RoomCode = "WD.1.304" }
-            };
+            //rooms = new List<Room>
+            //{
+            //    new Room
+            //    {
+            //        Id = 1,
+            //        Name = "H.4.104",
+            //        Reservations = new List<Reservation>
+            //        {
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 05, 12, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" },
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 05, 04, 12, 10, 0), Length = 2, Reserver = "MINUTO", Subject = "INFLAB-01" },
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 05, 14, 00, 0), Length = 2, Reserver = "LAFLEJ", Subject = "TESTING" },
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 03, 05, 12, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" },
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 08, 12, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" }
+            //        }
+            //    },
+            //    new Room
+            //    {
+            //        Id = 2,
+            //        Name = "H.4.204",
+            //        Reservations = new List<Reservation>
+            //        {
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2010, 04, 05, 11, 10, 0), Length = 2, Reserver = "COSTG", Subject = "INFDTA02-1" },
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2017, 05, 04, 12, 10, 0), Length = 2, Reserver = "MINUTO", Subject = "INFLAB-01" },
+            //            new Reservation {RoomID = 1, DateAndTime = new DateTime(2018, 04, 05, 14, 00, 0), Length = 2, Reserver = "LAFLEJ", Subject = "TESTING" },
+            //        },
+            //    },
+            //    new Room { Id = 3, Name = "H.3.304" },
+            //    new Room { Id = 4, Name = "WD.3.304" },
+            //    new Room { Id = 5, Name = "H.3.305" },
+            //    new Room { Id = 6, Name = "WD.1.304" }
+            //};
+            rooms = APIConnection.GetAllRooms("http://145.24.222.103:8080/manage/getallrooms");
         }
 
 
@@ -59,9 +60,14 @@ namespace ict_lab_website.Models
             rooms.Remove(room);
         }
 
-        public Room GetById(int id)
+        public Room GetById(string id)
         {
-            return rooms.Where(room => room.ID == id).First();
+            return rooms.Where(room => room.Id.Equals(id)).First();
+        }
+
+        public Room GetByName(string name)
+        {
+            return rooms.Where(room => room.Name.Equals(name)).First();
         }
     }
 }
