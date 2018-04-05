0eb8b664492f841b0ddfc809cfc5dc602f6097e3
Wouterkop1993 <0919023@hr.nl>
2018-04-05 16:49:43 +0200

    Added class for posting data to api and receiving true or false back

--- ict-lab-website/Models/Register.cs
+++ ict-lab-website/Models/Register.cs
@@ -3,10 +3,7 @@ namespace ict_lab_website.Models
 {
     public class Register
     {
-        string Firstname;
-        string Surname;
         string Email;
         string Password;
-        string ConfirmPassword;
     }
 }
