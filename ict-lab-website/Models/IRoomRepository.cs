8587c34b41020709e166948b94726d7167b60a7a
Jeroen <jeroenlafleur@gmail.com>
2018-04-05 16:50:39 +0200

    Add function to get room by name from repository

--- ict-lab-website/Models/IRoomRepository.cs
+++ ict-lab-website/Models/IRoomRepository.cs
@@ -8,8 +8,10 @@ namespace ict_lab_website.Models
     public interface IRoomRepository
     {
         IQueryable<Room> Rooms { get; }
-        Room GetById(int id);
+        Room GetById(string id);
+        Room GetByName(string name);
         void Add(Room room);
         void Delete(Room room);
+
     }
 }
