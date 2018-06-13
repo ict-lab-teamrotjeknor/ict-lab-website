80d8268c37aaf7028d5ced31642536fafd23292a
Wouterkop1993 <0919023@hr.nl>
2018-06-13 17:24:21 +0200

    Added Unittest(getallusers)

---
+++ :Users:wouterkop:Desktop:ict-lab-website:ict_lab_website.Tests:Users:UsersTests.cs
@@ -0,0 +1,43 @@
+﻿using ict_lab_website.Process;
+using ict_lab_website.Tests.Users;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ict_lab_website.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ictlabwebsite.Controllers;
+using ict_lab_website.Tests.Fake_implementations;
+using ict_lab_website.Tests.Rooms;
+using ict_lab_website.Models.Users;
+
+namespace ict_lab_website.Tests.Users
+{
+    public class UsersTests
+    {
+		private readonly ILogger<UsersController> logger;
+        private readonly IOptions<ApiConfig> apiConfig;
+        private readonly IApiCalls failingApiCalls;
+        private readonly IApiCalls usersApiCalls;
+
+        public UsersTests()
+        {
+			this.apiConfig = new FakeIOptions();
+			this.logger = new FakeLogger<UsersController>();
+            this.failingApiCalls = new FakeFailingApiCalls();
+			this.usersApiCalls = new FakeRoomsApiCalls();
+        }
+		[Fact]
+		public void GetAllUsers_ShouldWork()
+		{
+			IUsers usersRepository = new UsersRepository(apiConfig, logger);
+		}
+
+		[Fact]
+        public void GetAllUsers_ShouldReturnEmptyStringWithNotWorkingApiCalls()
+		{
+			
+		}
+    }
+}
