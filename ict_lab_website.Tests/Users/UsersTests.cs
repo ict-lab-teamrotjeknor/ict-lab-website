65a7e8f126ce23f73d8d4b8270ddc5c663badd70
Wouterkop1993 <0919023@hr.nl>
2018-06-14 10:31:31 +0200

    test

--- ict_lab_website.Tests/Users/UsersTests.cs
+++ ict_lab_website.Tests/Users/UsersTests.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 namespace ict_lab_website.Tests.Users
 {
-    public class UsersTests
-    {
-        public UsersTests()
-        {
-        }
-    }
+	public class UsersTests
+	{
+		private readonly ILogger<UsersController> logger;
+		private readonly IOptions<ApiConfig> apiConfig;
+		private readonly IApiCalls failingApiCalls;
+		private readonly IApiCalls usersApiCalls;
+
+		public UsersTests()
+		{
+
+			this.apiConfig = new FakeIOptions();
+			this.logger = new FakeLogger<UsersController>();
+			this.failingApiCalls = new FakeFailingApiCalls();
+			this.usersApiCalls = new FakeRoomsApiCalls();
+		}
+
+		[Fact]
+		public void GetAllUsers_ShouldWork()
+		{
+			IUsers usersRepository = new UsersRepository(apiConfig, logger, usersApiCalls);
+			string users = usersRepository.GetAllUsers();
+
+			//Check if type is a string which should be true
+			Assert.IsType<string>(users);
+
+			//Check if string is empty which should be false 
+			Assert.False(users.Equals(""));
+		}
+
+	}
 }
