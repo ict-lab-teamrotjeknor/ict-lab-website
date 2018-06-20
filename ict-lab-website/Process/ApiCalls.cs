1ea070900bc8fc14decca326e0ba10a9e3629f45
Wouterkop1993 <0919023@hr.nl>
2018-06-20 09:57:28 +0200

    Remove

--- ict-lab-website/Process/ApiCalls.cs
+++ ict-lab-website/Process/ApiCalls.cs
@@ -12,7 +12,6 @@ namespace ict_lab_website.Process
 {
     public class ApiCalls : IApiCalls
     {
-        //public HttpResponse httpResponse;
         public JObject PostRequest(JObject postData, string url)
         {
             var result = new JObject();
@@ -33,12 +32,6 @@ namespace ict_lab_website.Process
                 var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
                 using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
-                    var test = httpResponse.Headers.Get(6);
-
-                    //Cookie cookie = new Cookie();
-                    //cookie.Name = "hoi";
-                    //httpResponse.Cookies.Add(cookie);
-                    //tester = httpContextAccessor.HttpContext.Request.Cookies[test];
                     result = JObject.Parse(streamReader.ReadToEnd());
                 }
             }
